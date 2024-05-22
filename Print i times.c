660d0556207dfb0f6a7ea93d431d6c16df68d4fd
Humayera Tabassum Tunan <136461341+Tuunaan@users.noreply.github.com>
2024-05-22 10:46:22 +0600

    Add files via upload

---
+++ Print i times.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{int i,j,k,rum,sum,n;
+    printf("Enter:");
+    scanf("%d", &i);
+ for(j=2*i; j!=0; j--)
+    {printf("(%d) I am a CSE Student\n", j);}}
